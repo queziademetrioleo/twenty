c73225defc605aba71fc1b1cd8760fd77f6413d9
Quézia <queziademetrio@gmail.com>
2026-06-23 19:51:19 -0300

    fix: usar @/utils/validation em vez de self-import twenty-shared/utils

--- packages/twenty-shared/src/utils/filter/dates/utils/resolveRelativeDateFilter.ts
+++ packages/twenty-shared/src/utils/filter/dates/utils/resolveRelativeDateFilter.ts
@@ -3,7 +3,7 @@ import { getNextPeriodStart } from '@/utils/filter/dates/utils/getNextPeriodStar
 import { getPeriodStart } from '@/utils/filter/dates/utils/getPeriodStart';
 import { type RelativeDateFilter } from '@/utils/filter/dates/utils/relativeDateFilterSchema';
 import { subUnitFromZonedDateTime } from '@/utils/filter/dates/utils/subUnitFromZonedDateTime';
-import { isDefined } from 'twenty-shared/utils';
+import { isDefined } from '@/utils/validation';
 import { type Temporal } from 'temporal-polyfill';
 
 // TODO: use this in workflows where there is duplicated logic
